19ec4bb216ccd8f8d0bc48db20f977d2b763909f
helmi75 <51704295+helmi75@users.noreply.github.com>
2021-05-21 23:36:35 +0200

    Update traiding_streamlit_V3.py

--- traiding_streamlit_V3.py
+++ traiding_streamlit_V3.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# -*- coding: utf-8 -*-
+
+
 import pandas as pd
 import os
 import numpy as np
@@ -12,117 +15,138 @@ import plotly.express as px
 import streamlit as st
 import plotly.graph_objects as go
 import base64
-import ccxt
+
+from config import *
 from fonctions import *
-import time as tm
+
+def choix_market():
+  liste_crypto= np.array(['BTC/USDT', 'ETH/USDT', 'ADA/USDT','DOGE/USDT', 'BNB/USDT', 'UNI/USDT',
+                    'LTC/USDT', 'BCH/USDT', 'LINK/USDT', 'VET/USDT', 'XLM/USDT', 'FIL/USDT','TRX/USDT', 
+                    'NEO/USDT','EOS/USDT','DOT/USDT'])
+  cols3 = st.beta_columns(3)    
+  btc = cols3[0].checkbox('BTC/USDT')
+  eth = True #cols3[0].checkbox('ETH/USDT')
+  ada = cols3[0].checkbox('ADA/USDT')
+  doge = cols3[0].checkbox('DOGE/USDT')
+  bnb = cols3[0].checkbox('BNB/USDT')
+  uni = cols3[1].checkbox('UNI/USDT')
+  bch = cols3[1].checkbox('BCH/USDT')
+  link = cols3[1].checkbox('LINK/USDT')
+  vet = cols3[1].checkbox('VET/USDT')
+  xml = cols3[1].checkbox('XLM/USDT')
+  fil = cols3[2].checkbox('FIL/USDT')
+  ltc = cols3[2].checkbox('LTC/USDT')
+  trx = cols3[2].checkbox('TRX/USDT')
+  neo = cols3[2].checkbox('NEO/USDT')
+  eos = cols3[2].checkbox('EOS/USDT')
+  dot = cols3[2].checkbox('DOT/USDT')
+      
+  liste_boolean = np.array([btc, eth, ada, doge, bnb, uni,
+                     ltc, bch, link, vet, xml, fil, trx, neo, eos, dot])    
+  return liste_crypto[liste_boolean]
+
+def plot_courbes(crypto, tableau_var):
+    fig=go.Figure()
+    for elm in crypto: 
+        
+        fig.add_trace(go.Scatter(x= crypto[elm].index, 
+                                 y= crypto[elm]['cumul_'+elm[:3]],
+                                 mode='lines',
+                                 name=elm[:3],
+                                 ))  
+    fig.add_trace(go.Scatter(x= tableau_var.index, 
+                                 y= tableau_var['coef_multi'],
+                                 mode='lines',
+                                 name='coef_multi_BX1',
+                                 )) 
+    fig.add_trace(go.Scatter(x= tableau_var.index, 
+                                 y= tableau_var['coef_cumul'],
+                                 mode='lines',
+                                 name='coef_cumul_BX1',
+                                 ))
+    fig.update_layout(
+    title="Variation cumulées ",
+    xaxis_title="date",
+    yaxis_title="cumul ",
+    legend_title="cryptos",
+    )
+    return st.plotly_chart(fig)
 
 
-def pipeline_crypto(market, exchange, delta_hour):
+def plot_courbes2(tableau_var):
+    fig=go.Figure()
      
-     crypto ={}
-     for elm in market :
-            x =elm.lower()
-            ohlcv = exchange.fetch_ohlcv(elm ,limit = 1000, timeframe = delta_hour)
-            crypto[x] = pd.DataFrame(ohlcv,columns=['timestamp', x[:3]+'_open', 'high','low', x[:3]+'_close', 'volume'])
-            crypto[x] = convert_time(crypto[x])
-            crypto[x] = crypto[x][['timestamp',x[:3]+'_open',x[:3]+'_close']] 
-            crypto[x] = crypto[x].set_index('timestamp')
-            crypto[x] = crypto[x].merge(variation(crypto[x]),on ='timestamp',how='left')
-            crypto[x]['coef_multi_'+x[:3]]=coef_multi(crypto[x])
-            crypto[x]  = fonction_cumul(crypto[x],x)
-     return crypto
- 
+    for elm in tableau_var.iloc[:,:-5]:
+        tableau_var[elm]       
+        fig.add_trace(go.Scatter(x= tableau_var[elm].index, 
+                                 y= tableau_var[elm],
+                                 mode='lines',
+                                 name=elm,
+                                 )) 
+    return st.plotly_chart(fig)
 
-
-
-market = choix_market()
+def main():
+    
+    star_time = to_timestamp(str(st.date_input('date de début',date_init ))) #1502928000000
+    end_time = to_timestamp(str(st.date_input('date de fin')))
+    delta_hour = '4h'
+    #st.selectbox('selectionner une plage auraire',
+                 #'4h','6h','8h','12h'])
+   
+    
+      
+    crypto = {}
+    boxmax ={}
+    market= choix_market()
+    
+    for elm in market :
+        x =elm.lower()   
+        crypto[x] = down_all_coin(elm ,star_time, end_time,delta_hour,exchange) #
+        crypto[x]=pd.DataFrame(data=crypto[x], columns=['timestamp', x[:3]+'_open', 'high','low', x[:3]+'_close', 'volume']) 
+        crypto[x] = crypto[x][['timestamp',x[:3]+'_open',x[:3]+'_close']]     
+        #crypto[x] = convert_time(crypto[x])
+        crypto[x] = crypto[x].set_index('timestamp')
+    
+    
+    array_mauvais_shape = detection_mauvais_shape(crypto)
+    crypto = correction_shape(crypto, array_mauvais_shape)
+    for elm in array_mauvais_shape :
+        crypto[elm]['timestamp'] = generation_date (crypto[elm], int(delta_hour[:1]))
+        crypto[elm] =  crypto[elm].set_index('timestamp') 
+    for elm in market : 
+        x =elm.lower() 
+        crypto[x] = crypto[x].merge(variation(crypto[x]),on ='timestamp',how='left')
+        crypto[x]['coef_multi_'+x[:3]]=coef_multi(crypto[x])
+        crypto[x]  = fonction_cumul(crypto[x],x) 
+        
+        
+        
+        
+        
+    df_liste_var =  fonction_tableau_var(crypto)   
+    tableau_var = meilleur_varaition(df_liste_var) 
+    
+    tableau_var['algo'] = algo(tableau_var)
+    tableau_var['coef_multi'] = tableau_var['algo'].cumprod()
+    tableau_var['coef_cumul']= tableau_var['coef_multi'].apply(lambda x : (x*100)-100)
+   
+    plot_courbes(crypto, tableau_var)
+    st.write(tableau_var)
+    
+   
+    
+    
+   
 
     
-apiKey = st.sidebar.text_input("Enter Apikey", type="password")
-secret = st.sidebar.text_input("Enter a Secretkey", type="password")
-
-
-
-dth= st.sidebar.selectbox('delta_hour',['1m', '5m','15m', '1h','4h','6h','8h','12h','1d'], index = 5)
-
-
-date_init = datetime.now() - timedelta(days = 30)         
-star_time = to_timestamp(str(st.sidebar.date_input('date de début',date_init ))) #1502928000000
-end_time = to_timestamp(str(st.sidebar.date_input('date de fin')))
-
-delta_hour = dth
-
-
-exchange = ccxt.binance({
-    'apiKey': apiKey,
-    'secret': secret,
-    'enableRateLimit': True
-    })
-
-# initialisation temps 
-
-start_time = datetime.now()
-k=0
-liste_principale=[]
-liste_achat=[]
-liste_vente=[]  
-temps=[]
-
-        
-crypto = pipeline_crypto(market, exchange, delta_hour)
-
-
-df_liste_var =  fonction_tableau_var(crypto)  
-tableau_var = meilleur_varaition(df_liste_var)
-tableau_var = meilleur_varaition(df_liste_var) 
-tableau_var['algo'] = algo(tableau_var)
-tableau_var['coef_multi'] = tableau_var['algo'].cumprod()
-tableau_var['coef_multi'] = (tableau_var['coef_multi']*100)-100
-plot_courbes(crypto, tableau_var)
-
- 
-st.write(tableau_var )
-print(tableau_var['coef_multi'] )
-
-
-
-bouton_run = st.button("run")
-if bouton_run:
-    while True:
-
-      
-
-        st.write("première iteration  : ",start_time) 
-        st.write("horaire now", datetime.now())
-        st.write("iteration numéto : ", k)
-
-# =============================================================================
-#         
-#         tableau_var = meilleur_varaition(df_liste_var) 
-#         tableau_var['algo'] = algo(tableau_var)
-#         tableau_var['coef_multi'] = tableau_var['algo'].cumprod()
-#         nom_crypto_achat =  nom_crypto_achat_vente(tableau_var)
-#         nom_crypto_vente= crypto_a_vendre(exchange,2, market ) 
-#         
-#         algo_achat_vente(exchange, nom_crypto_vente, nom_crypto_achat)
-#         
-#         print('la crypot à vendre est ',nom_crypto_vente)
-#         print('la crypot à acheter est ',nom_crypto_achat)
-# 
-# 
-#         k=k+1
-#         liste_principale.append([datetime.now(), nom_crypto_achat, nom_crypto_vente])
-#         st.write(tableau_var)
-#         st.write(pd.DataFrame(liste_principale, columns=['temps','crypto vente','crypto achat']))
-#         
-#         if nom_crypto_achat ==  nom_crypto_vente:
-#             st.write('On reste sur la même crypto')
-#         else:
-#             st.write('crypto à vendre ',nom_crypto_vente)
-#             st.write('crypto à acheter',nom_crypto_achat)
-#        
-# 
-# 
-#         tm.sleep(60*i_iteration)
-# =============================================================================
-
+   
+    
+    
+  
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
