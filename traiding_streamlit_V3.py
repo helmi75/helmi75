6014a2be6d318cb1618814db7f3275b076224055
helmi75 <51704295+helmi75@users.noreply.github.com>
2021-05-24 14:04:43 +0200

    Update traiding_streamlit_V3.py

--- traiding_streamlit_V3.py
+++ traiding_streamlit_V3.py
@@ -154,6 +154,7 @@ def main():
     plot_courbes2(df_tableau_multi)
     if st.checkbox('Voir tableau coef multi') :      
       st.write(df_tableau_multi)
+    st.bar_chart (df_tableau_multi.tail(1))
     st.write( df_tableau_multi.tail(1))
     if st.checkbox('Voir tableau de variation'):
        st.write(tableau_var)
